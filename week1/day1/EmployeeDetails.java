26f6678588447ea1577605256b47748442cc2342
shareentaj-r <tajshareen@gmail.com>
2025-10-06 07:47:27 +0530

    week1 day1 assignment

---
+++ week1/day1/EmployeeDetails.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		String name = "John Doe";
+		int	age = 30;
+		float salary = 55000.75f;
+		char gender = 'M';
+		boolean	acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210l;
+		int	pinCode = 56001;
+		int	luckyNumber = 7;
+
+		System.out.println("name=" +name);
+		System.out.println("age=" +age);
+		System.out.println("salary=" +salary);
+		System.out.println("gender=" +gender);
+		System.out.println("acceptTerms=" +acceptTerms);
+		System.out.println("rating=" +rating);
+		System.out.println("mobileNumber=" +mobileNumber);
+		System.out.println("pinCode=" +pinCode);
+		System.out.println("luckyNumber=" +luckyNumber);
+	}
+
+
+}
